df77717c4e31253a4cdf9a07f3fb48ee5ab11697
Steven- 19220600 <stevensquad454@gmail.com>
2023-09-25 12:05:18 +0700

    bukucontroller

---
+++ app/Controllers/bukucontroller.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controllers;
+
+class BukuController extends BaseController
+{
+    public function daftarbuku()
+    {
+        
+    }
+
+    public function detailbuku ()
+    {
+        
+    }
+
+    public function menambahakanbuku ()
+    {
+
+    }
+
+    public function mengeditbuku ()
+    {
+
+    }
+
+    public function menhapusbuku ()
+    {
+        
+    }
+
+}
