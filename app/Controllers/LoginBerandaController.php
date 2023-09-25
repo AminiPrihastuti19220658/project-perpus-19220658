af4df841139110d454ed334930572be4e904bfe8
aminiprhsti <145204307+aminiprhsti@users.noreply.github.com>
2023-09-25 12:20:25 +0700

    controller login

---
+++ app/Controllers/LoginBerandaController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class Login extends BaseController
+{
+    public function Daftar()
+    {
+        
+    }
+
+    public function Masuk()
+    {
+
+    }
+
+    public function Lupa_Katasandi()
+    {
+
+    }
+}
+
+class Beranda extends BaseController
+{
+    public function Halaman_Dinamis()
+    {
+
+    }
+
+    public function Sistem_Komentar()
+    {
+
+    }
+
+    public function Pencarian()
+    {
+
+    }
+
+    public function Kontrol_Akses_Pengguna()
+    {
+
+    }
+}
