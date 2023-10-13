abdfdac1e6b3972f2822865376523a240decf23f
Ratri19220659 <ratriwahyuningsih93@gmail.com>
2023-10-13 19:15:55 +0700

    buku CRUD

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,15 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+$routes->get('/buku', 'BukuController::index');
+$routes->get('/buku/form', 'BukuController::form');
+$routes->get('/buku/edit/(:num)', 'BukuController::edit/$1');
+$routes->post('/buku/create', 'BukuController::create');
+$routes->post('/buku/hapus', 'BukuController::hapus');
+
+$routes->get('/koleksibuku', 'KoleksiBukuController::index');
+$routes->get('/koleksibuku/form', 'KoleksiBukuController::form');
+$routes->get('/koleksibuku/edit/(:num)', 'KoleksiBukuController::edit/$1');
+$routes->post('/koleksibuku/create', 'KoleksiBukuController::create');
+$routes->post('/koleksibuku/hapus', 'KoleksiController::hapus');
